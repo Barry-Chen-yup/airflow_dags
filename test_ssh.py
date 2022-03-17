2b30964d1f15080a99825dc9b5c9e0aa4f80b86d
workspacebarry <akj00173@gmail.com>
2022-03-17 16:17:06 +0000

    ssh_containerv5

--- test_ssh.py
+++ test_ssh.py
@@ -36,7 +36,7 @@ with DAG(
     ssh = SSHOperator(
         task_id='ssh',
         ssh_conn_id='workspace',
-        command='sudo python /workspace/tracker/code/track_chicken_date_gpu.py /workspace/nas-data/Animal/chicken_video/20210131',
+        command='python /workspace/tracker/code/track_chicken_date_gpu.py /workspace/nas-data/Animal/chicken_video/20210131 ',
     ) 
     # python_timestamp = PythonOperator(
     #     task_id='python',
